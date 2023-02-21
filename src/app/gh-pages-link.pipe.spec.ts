166b6dc4f70c60d4147ed3602af47de515f03856
JustinPaoletta <justinpaoletta@gmail.com>
2023-02-21 17:27:58 -0500

    - deletes duplicate pipe

--- src/app/gh-pages-link.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GhPagesLinkPipe } from './gh-pages-link.pipe';
-
-describe('GhPagesLinkPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GhPagesLinkPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
