39160a0e48c492fd622db074707c6160e67778f8
JustinPaoletta <justinpaoletta@gmail.com>
2023-02-27 22:41:01 -0500

    remove geolocation code

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -15,26 +15,6 @@ export class ProjectsComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.get("https://geolocation-db.com/json/").subscribe((r) => console.log(r))
-
-    window.navigator.geolocation.getCurrentPosition((pos) => {
-      let isValidLat = false;
-      let isValidLong = false;
-
-      if (pos.coords.latitude > 28.57255 && pos.coords.latitude < 28.57267) {
-        isValidLat = true;
-      }
-
-      if (pos.coords.longitude < -81.3060 && pos.coords.longitude > -81.30606) {
-        isValidLong = true;
-      }
-
-      console.log(pos.coords.latitude)
-      console.log(isValidLat)
-      console.log(pos.coords.longitude)
-      console.log(isValidLong)
-      console.log(isValidLat && isValidLong)
-      
-    });
   }
 
   todo = ['Stoic'];
