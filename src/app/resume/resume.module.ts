b218feb4d0eea7c60eb0ac726452eb2d820a3f4c
JustinPaoletta <justinpaoletta@gmail.com>
2023-03-06 22:51:17 -0500

    - adds ng2-pdf-viewer to display resume - deletes projects.png

--- src/app/resume/resume.module.ts
+++ src/app/resume/resume.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ResumeComponent } from './resume.component';
 import { CloseModule } from '../close/close.module';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { MatIconModule } from '@angular/material/icon';
+import { GhPagesLinkPipeModule } from "../pipes/gh-pages-link.module";
 
 @NgModule({
     declarations: [ResumeComponent],
-    imports: [CommonModule, CloseModule],
+    imports: [CommonModule, CloseModule, PdfViewerModule, MatIconModule, GhPagesLinkPipeModule]
 })
 export class ResumeModule {}
