5a0e7a9109fbe774bbfcc43e48ee5b0ca3ace104
JustinPaoletta <justinpaoletta@gmail.com>
2023-03-13 20:24:23 -0400

    - added photos

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -5,9 +5,10 @@ import { AboutComponent } from './about.component';
 import { CloseModule } from '../close/close.module';
 import { GhPagesLinkPipeModule } from "../pipes/gh-pages-link.module";
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
     declarations: [AboutComponent],
-    imports: [CommonModule, CloseModule, GhPagesLinkPipeModule, MatCardModule]
+    imports: [CommonModule, CloseModule, GhPagesLinkPipeModule, MatCardModule, MatIconModule]
 })
 export class AboutModule {}
