ea3995ee0e4887fe7da0118baacbcc59075c2f96
JustinPaoletta <justinpaoletta@gmail.com>
2023-03-21 19:08:00 -0400

    - set chrome

--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,6 @@ process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
     config.set({
-        browsers: ['ChromeHeadless']
+        browsers: ['Chrome']
     })
 }
